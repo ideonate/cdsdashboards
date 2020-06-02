cfb6bd1f4e6de2f0d1121c5efb61ef385253998b
Dan Lester <dan@danlester.com>
2020-06-02 20:03:23 +0100

    0.0.14 in setup

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     name='cdsdashboards',
     packages=find_packages(),
     package_data=get_package_data(),
-    version='0.0.13',
+    version='0.0.14',
     python_requires='>=3.6',
     author='Ideonate',
     author_email='dan@containds.com',
