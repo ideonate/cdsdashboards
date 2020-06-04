2c7f80ac5f521bd432b03b4de7dc40a38724dda1
Dan Lester <dan@danlester.com>
2020-06-04 18:31:00 +0100

    Tidy script

--- e2e/run_all.sh
+++ e2e/run_all.sh
@@ -34,24 +34,6 @@ do
 done
 
 
-#./localprocess_test.sh
-
-#./localprocess_test_db13upgrade.sh
-
-#./localprocess_test_db11upgrade.sh
-
-#./localprocess_voila.sh
-
-
-#./tljh_test.sh
-
-#./tljh_test_db13upgrade.sh
-
-#./tljh_test_db11upgrade.sh
-
-#./tljh_voila.sh
-
-
 echo ""
 echo "Any Screenshots of failures:"
 echo "----------------------------"
