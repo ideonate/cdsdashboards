a186fe4b75e240d46480bb2a43f4eb5e531a8be7
Dan Lester <dan@ideonate.com>
2021-06-18 10:04:45 +0100

    Cypress test only new dbupgrade

--- e2e/functest_db057upgrade.sh
+++ e2e/functest_db057upgrade.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export JH_CYPRESS_SQLITE_SRC="/jh_cypress_config/from.0.0.11.sqlite"
+export JH_CYPRESS_SQLITE_SRC="/jh_cypress_config/from.0.5.7.sqlite"
 
 export JH_CYPRESS_TESTS="/e2e/cypress/integration/dbupgrade.spec.js"
 
